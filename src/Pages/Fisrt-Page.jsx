93cd293f2eefbafaeb396fb372ef09310859747c
aqdarahmad <a.abusufyan@student.aaup.edu>
2025-08-22 05:11:06 +0300

    create home-page

--- src/Pages/Fisrt-Page.jsx
+++ src/Pages/Fisrt-Page.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../Components/Navbar/Navbar";
 import { useEffect, useState } from "react";
+import './pagestyle.css'
 
 export default function Fisrt_Page() {
     const [photos, setPhotos] = useState([]);
@@ -7,20 +8,21 @@ export default function Fisrt_Page() {
     useEffect(() => {
         fetch("http://localhost:4000/photos").then(res => res.json()).then(data => setPhotos(data))
     }, []);
-    
+
     return (
         <div>
             <Navbar />
-            <h1>Gallery</h1>
-            <div>
-                {photos.map(photo => (
-                    <div key={photo.id}>
-                        <img src={photo.src} alt={photo.title} />
-                        <p>{photo.title}</p>
+            <h1 className="gallery-title">Gallery</h1>
 
+            <div className="gallery-grid">
+                {photos.map((photo) => (
+                    <div className="gallery-card" key={photo.id}>
+                        <img className="gallery-img" src={photo.src} alt={photo.title} />
+                        <p className="gallery-text">{photo.title}</p>
                     </div>
                 ))}
             </div>
         </div>
-    )
+    );
+
 }
